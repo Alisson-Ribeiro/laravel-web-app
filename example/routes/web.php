1c9e831495df54351ca957bc931d83960138801e
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2024-04-24 13:37:47 -0300

    create new job feature and validation

--- example/routes/web.php
+++ example/routes/web.php
@@ -4,22 +4,46 @@ use Illuminate\Support\Facades\Route;
 use App\Models\job;
 
 Route::get('/', function () {
+    
     return view('index');
 });
 
 
 Route::get('/jobs', function () {
-    $jobs = job::with('employer')->paginate(5);
-    return view('jobs', [
+    $jobs = job::with('employer')->latest()->paginate(5);
+    
+    return view('jobs/index', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function() {
+    
+    return view('jobs/create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
     $job = job::find($id);
-    return view ('job', ['job' => $job]);
+    
+    return view ('jobs/show', ['job' => $job]);
+});
+
+Route::post('/jobs', function(){
+    request()->validate([
+        'title' => 'required|min:3',
+        'salary' => 'required',
+    ]);
+    
+    job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
+    
     return view('contact');
 });
