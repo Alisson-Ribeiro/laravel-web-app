bef8c3a8892fd21cd195a9dce035411272f11548
Alisson-Ribeiro <alisson-ohkamy@hotmail.com>
2024-04-26 15:00:37 -0300

    new features added - update | delete | edit

--- example/routes/web.php
+++ example/routes/web.php
@@ -17,17 +17,20 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// CREATE
 Route::get('/jobs/create', function() {
     
     return view('jobs/create');
 });
 
+// SHOW
 Route::get('/jobs/{id}', function ($id) {
     $job = job::find($id);
     
     return view ('jobs/show', ['job' => $job]);
 });
 
+// STORE
 Route::post('/jobs', function(){
     request()->validate([
         'title' => 'required|min:3',
@@ -41,6 +44,49 @@ Route::post('/jobs', function(){
     ]);
 
     return redirect('/jobs');
+});
+
+// EDIT
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = job::find($id);
+    
+    return view ('jobs/edit', ['job' => $job]);
+});
+
+// UPDATE
+Route::patch('/jobs/{id}', function ($id) {
+   
+    // validate
+    request()->validate([
+        'title' => 'required|min:3',
+        'salary' => 'required',
+    ]);
+
+    // authorize ( On hold...)
+
+    // update
+    $job = job::findOrFail($id);
+
+    $job->update([
+        'title'=>request('title'),
+        'salary'=>request('salary'),
+    ]);
+
+    // redirect to the job page
+    return redirect('/jobs/'. $job->id);
+
+});
+
+// DELETE
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize ( On hold...)
+
+    // delete the job
+    $job = job::findOrFail($id)->delete();
+
+    // redirect
+    return redirect('/jobs');
+
 });
 
 Route::get('/contact', function () {
